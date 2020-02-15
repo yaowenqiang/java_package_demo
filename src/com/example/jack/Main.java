47418fe2a5159d74ac140a626a199cce723ca102
yaowenqiang <yaowenqiang111@163.com>
2020-02-15 23:36:10 +0800

    add Pakcages Demo

---
+++ src/com/example/jack/Main.java
@@ -0,0 +1,10 @@
+package com.example.jack;
+
+public class Main {
+//    https://docs.oracle.com/javase/tutorial/java/generics/types.html
+//    https://docs.oracle.com/javase/specs/jls/se6/html/packages.html
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow("Complete Java.");
+        myWindow.setVisible(true);
+    }
+}
